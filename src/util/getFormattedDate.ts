843e2ade5b55b984e65b372ac2e8739a933750ce
heajun jeong <heajun35@gmail.com>
2024-07-30 14:53:59 +0900

    [Feat] - 기초세팅

--- src/util/getFormattedDate.ts
+++
@@ -1,8 +0,0 @@
-export const getFormattedDate = (date: string | Date) =>
-  new Date(date).toLocaleDateString("ko", {
-    year: "2-digit",
-    month: "2-digit",
-    day: "2-digit",
-    hour: "2-digit",
-    minute: "2-digit",
-  });
