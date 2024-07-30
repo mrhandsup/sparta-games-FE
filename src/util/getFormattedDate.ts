32b8a4ad1cd4223ffa26f01050d7dfafc211d8ec
heajun jeong <heajun35@gmail.com>
2024-07-30 14:54:20 +0900

    [Feat] - 날짜 변경 함수 저장

---
+++ src/util/getFormattedDate.ts
@@ -0,0 +1,8 @@
+export const getFormattedDate = (date: string | Date) =>
+  new Date(date).toLocaleDateString("ko", {
+    year: "2-digit",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+  });
