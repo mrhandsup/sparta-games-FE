9c122c64d335a26867a0afc3e4bafde8d6777d5c
DecidedCard <qube7089@naver.com>
2024-08-07 20:21:40 +0900

    [Feat:#1] - footer 위치 지정

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,11 +1,16 @@
 import { Outlet } from "react-router-dom";
+
 import Header from "../Header";
+import Footer from "../Footer";
 
 const Layout = () => {
   return (
-    <div className="mx-auto max-w-[1440px] min-h-screen border border-solid border-black">
+    <div className="relative mx-auto max-w-[1440px] min-h-screen border border-solid border-black">
       <Header />
       <Outlet />
+      <div className="absolute bottom-0">
+        <Footer />
+      </div>
     </div>
   );
 };
