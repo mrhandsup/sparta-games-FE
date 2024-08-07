d6f1849ce20b46a751874eee82a700e85c509c70
DecidedCard <qube7089@naver.com>
2024-08-07 23:15:14 +0900

    [Feat:#4] - Game Card 생성

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,10 +1,17 @@
 import Hero from "../components/HomeComponents/Hero";
+import GameCard from "../components/HomeComponents/GameCard";
 
 const Home = () => {
   return (
-    <div>
+    <main>
       <Hero />
-    </div>
+      <div>
+        <p className="flex mx-auto w-[1180px] h-12 text-5xl font-bold">지금 가장 인기있는 게임</p>
+        <div>
+          <GameCard />
+        </div>
+      </div>
+    </main>
   );
 };
 
