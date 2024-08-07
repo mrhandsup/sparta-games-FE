649acc1d547b8b0eb73439512f98587f66f06ec4
DecidedCard <qube7089@naver.com>
2024-08-07 23:30:56 +0900

    [Feat:#4] - 메인 페이지 게임카드 구조 작성

--- src/components/HomeComponents/GameCard.tsx
+++ src/components/HomeComponents/GameCard.tsx
@@ -1,5 +1,24 @@
 const GameCard = () => {
-  return <div>GameCard</div>;
+  return (
+    <section>
+      <div className="relative flex justify-center items-center w-[279px] h-[245px] bg-gray-200">
+        이미지<p className="absolute top-0 left-0">Categories</p>
+      </div>
+      <div className="p-3 w-[279px] h-[163px]">
+        <div className="flex justify-between items-center">
+          <p className="text-xl font-bold">게임명</p>
+          <p>⭐⭐⭐⭐⭐</p>
+        </div>
+        <div className="text-sm font-normal">제작자</div>
+        <div className="text-sm font-medium">
+          세줄설명까지 가능 Hipster ipsum tattooed brunch I'm baby. Meggings keffiyeh hipster fixie loko.
+        </div>
+        <div className="flex gap-1">
+          <p>chip🎮</p>chip🎮<p></p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default GameCard;
--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -5,9 +5,12 @@ const Home = () => {
   return (
     <main>
       <Hero />
-      <div>
+      <div className="flex flex-col justify-evenly items-center w-full h-[536px]">
         <p className="flex mx-auto w-[1180px] h-12 text-5xl font-bold">지금 가장 인기있는 게임</p>
-        <div>
+        <div className="flex justify-between w-[1180px] h-[408px]">
+          <GameCard />
+          <GameCard />
+          <GameCard />
           <GameCard />
         </div>
       </div>
