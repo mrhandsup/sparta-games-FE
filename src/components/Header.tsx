d23a892e5e0d50c1b04a240fbb16e24ed4197d7f
DecidedCard <qube7089@naver.com>
2024-08-18 11:53:52 +0900

    [Style:#7] - haader 디자인 변경으로 인한 css 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,16 @@
 import searchIcon from "../assets/common/search.svg";
+import alert from "../assets/common/alert.svg";
 
 const Header = () => {
   return (
-    <header className="flex justify-between items-center py-5 px-[30px] w-full h-20 bg-white">
+    <header className="flex justify-between items-center py-5 px-[30px] w-full h-20 bg-gray-800 text-white">
       <section className="flex items-center gap-4">
         <img src="" alt="스파르타 게임 아이콘" className="w-12 h-12 rounded-full" />
-        <h1 className="text-5xl font-bold">Sparta Games</h1>
+        <h1 className="text-heading-32 text-primary-500">Sparta Games</h1>
       </section>
       <section className="flex items-center gap-10">
-        <form>
-          <div className="flex p-1 border border-solid border-black rounded-lg">
-            <input type="text" />
-            <img src={searchIcon} alt="검색 아이콘" />
-          </div>
-        </form>
+        <img src={searchIcon} alt="검색 아이콘" />
+        <img src={alert} alt="알림 아이콘" />
         <p>카테고리</p>
         <p>게임 업로드</p>
         <p>커뮤니티</p>
