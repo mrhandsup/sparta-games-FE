23d72e53d389128cfb1544825da9cf12ba839c19
heajun jeong <heajun35@gmail.com>
2024-09-05 16:56:22 +0900

    [Style:#13] - header 및 footer 타이틀 이미지로 변경

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,12 @@
+import titleImage from "../assets/footerTitleImage.svg";
+
 const Footer = () => {
   return (
     <footer className="flex justify-center py-8 px-[130px] w-full bg-black text-gray-200">
       <div className="flex flex-col items-center gap-4">
-        <p className="font-DungGeunMo text-heading-40">Sparta Games</p>
+        <p>
+          <img src={titleImage} />
+        </p>
         <p>© 2024 Sparta Games. All Rights Reserved.</p>
         <div className="flex gap-[60px] text-title-18">
           <p>ABOUT</p>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import useModalToggle from "../hook/useModalToggle";
 
+import titleImage from "../assets/titleImage.svg";
 import balloon from "../assets/headerImage/balloon.svg";
 import speaker from "../assets/headerImage/speaker.svg";
 import Category from "./headerComponents/Category";
@@ -11,7 +12,9 @@ const Header = () => {
     <header className="flex justify-between items-center py-5 px-[30px] w-full h-20 bg-gray-800 font-DungGeunMo text-white">
       <section className="flex items-center gap-4">
         <img src="" alt="스파르타 게임 아이콘" className="w-12 h-12 rounded-full" />
-        <h1 className="text-heading-32 text-primary-500">Sparta Games</h1>
+        <h1>
+          <img src={titleImage} />
+        </h1>
       </section>
       <section className="flex items-center gap-10 text-heading-24 font-normal">
         <img src={balloon} alt="검색 아이콘" />
