b26835fde8b3a890fcafa2ac00874751caa906db
heajun jeong <heajun35@gmail.com>
2024-09-09 21:07:48 +0900

    [Feat:#11] - 커뮤니티 페이지 게시글 카테고리 navigartion 작성

--- src/page/Community.tsx
+++ src/page/Community.tsx
@@ -1,9 +1,11 @@
 import CardList from "../components/communityComponents/CardList";
+import Navigation from "../components/communityComponents/Navigation";
 
 const Community = () => {
   return (
     <main className="min-h-full">
       <CardList />
+      <Navigation />
     </main>
   );
 };
