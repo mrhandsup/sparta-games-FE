027c36d47908a864319fb437c6fe5d4cb5f7f411
heajun jeong <heajun35@gmail.com>
2024-09-09 21:14:07 +0900

    [Feat:#11] - 커뮤니티 페이지 게시글 필터 및 검색 작성 중

---
+++ src/components/communityComponents/SearchFilter.tsx
@@ -0,0 +1,15 @@
+const SearchFilter = () => {
+  return (
+    <section className="flex justify-between mt-[75px] px-[130px]">
+      <div className="flex gap-5">
+        <p>최근 게시순</p>
+        <p>공감 많은순</p>
+        <p>댓글 많은 순</p>
+        {/* <p>나의 게시글</p> */}
+      </div>
+      <div></div>
+    </section>
+  );
+};
+
+export default SearchFilter;
--- src/page/Community.tsx
+++ src/page/Community.tsx
@@ -1,11 +1,13 @@
 import CardList from "../components/communityComponents/CardList";
 import Navigation from "../components/communityComponents/Navigation";
+import SearchFilter from "../components/communityComponents/SearchFilter";
 
 const Community = () => {
   return (
     <main className="min-h-full">
       <CardList />
       <Navigation />
+      <SearchFilter />
     </main>
   );
 };
