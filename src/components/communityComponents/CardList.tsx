51ae48c767a689e21f06c0c9a426e37963da49c1
heajun jeong <heajun35@gmail.com>
2024-09-09 20:54:52 +0900

    [Feat:#11] - 커뮤니티 페이지 인기있는 게시글 작성

--- src/components/communityComponents/CardList.tsx
+++ src/components/communityComponents/CardList.tsx
@@ -1,10 +1,18 @@
+import Card from "./Card";
+
 const CardList = () => {
   return (
     <>
-      <h3 className="py-5 px-[130px]">
+      <h3 className="flex gap-3 py-5 px-[130px]">
         <img src="/assets/communityImage/pixelFire.svg" />
-        <p className="font-DungGeunMo text-title-22 text-white">지금 가장 인기있는 게시글</p>
+        <p className="font-DungGeunMo text-heading-32 text-white">지금 가장 인기있는 게시글</p>
       </h3>
+      <div className="flex gap-5 w-[1180px] mx-auto">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </>
   );
 };
