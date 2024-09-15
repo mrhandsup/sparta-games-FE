624d44f1cfd2c8d5f6a30b2eed818a9b8dfefb9e
heajun jeong <heajun35@gmail.com>
2024-09-15 20:52:36 +0900

    [Feat:#17] - 카테고리 상수 작성

---
+++ src/util/constance/category.ts
@@ -0,0 +1,16 @@
+const CATEGORY = [
+  "Action",
+  "Arcade",
+  "FPS",
+  "Platform",
+  "Rhythm",
+  "Survival",
+  "Adventure",
+  "Casual",
+  "Horror",
+  "Puzzle",
+  "RPG",
+  "Test",
+];
+
+export default CATEGORY;
