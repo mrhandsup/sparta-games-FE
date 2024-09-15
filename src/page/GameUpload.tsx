e8a95b149123974729f58818434ff147b14e5f13
heajun jeong <heajun35@gmail.com>
2024-09-15 19:18:50 +0900

    [Feat:#17] - 유의사항 컴포넌트 작성

--- src/page/GameUpload.tsx
+++ src/page/GameUpload.tsx
@@ -1,5 +1,17 @@
+import Note from "../components/gameUploadComponents/Note";
+
+import pixelMeteor from "../assets/homeImage/pixelMeteor.svg";
+
 const GameUpload = () => {
-  return <div>GameUpload</div>;
+  return (
+    <main>
+      <div className="flex gap-3 my-10 mx-auto w-fit text-[32px] leading-[125%] text-white font-DungGeunMo">
+        <img src={pixelMeteor} />
+        <p>게임을 업로드 합니다.</p>
+      </div>
+      <Note />
+    </main>
+  );
 };
 
 export default GameUpload;
