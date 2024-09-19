42b03232aac14ae73580e0a4867902c9d41846f1
heajun jeong <heajun35@gmail.com>
2024-09-19 17:42:40 +0900

    [Feat:#19] - 리뷰폼 작성

--- src/components/gameDetailComponents/Review.tsx
+++ src/components/gameDetailComponents/Review.tsx
@@ -4,6 +4,7 @@ const Review = () => {
   return (
     <section className="my-12">
       <p className="font-DungGeunMo text-[32px] text-white">Review</p>
+      {/* 로그인/회원가입 구현 후 유저정보가 있을경우 리뷰폼이 나오게 변경 */}
       <ReviewForm />
     </section>
   );
