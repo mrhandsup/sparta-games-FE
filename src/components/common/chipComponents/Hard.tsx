eae5471c22c2fa0431a77ebda480e2edd604459a
heajun jeong <heajun35@gmail.com>
2024-09-19 20:33:22 +0900

    [Style:#19] - Hard chip 스타일 수정

--- src/components/common/chipComponents/Hard.tsx
+++ src/components/common/chipComponents/Hard.tsx
@@ -1,9 +1,9 @@
 const Hard = ({ gap, bg }: { gap: number; bg?: boolean }) => {
   return (
     <div
-      className={`flex items-center gap-[${gap}px] p-[${gap}px] h-12 ${
+      className={`flex items-center gap-[${gap}px] p-[${gap}px] ${
         bg ? "bg-primary-800" : "bg-gray-600"
-      }  rounded-lg`}
+      } font-bold  rounded-lg`}
     >
       🎮 HARD
     </div>
