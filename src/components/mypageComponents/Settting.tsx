d39ae05ac6f514c933c120b4ad0b9f002118274b
horang-e <rmsid1000@gmail.com>
2024-11-02 23:07:31 +0900

    [Feat] setting update

--- src/components/mypageComponents/Settting.tsx
+++ src/components/mypageComponents/Settting.tsx
@@ -1,6 +1,4 @@
-import React from "react";
 import log from "../../assets/Log.svg";
-import ButtonComponent from "../common/SpartaButton";
 
 type Props = {};
 
@@ -18,10 +16,13 @@ const Settting = (props: Props) => {
         </div>
       </div>
       {/* 프로필 수정 */}
-      <div className="bg-gray-800 rounded-xl px-7 py-5 flex flex-col gap-4 justify-start items-start w-full">
-        <div className="flex items-center gap-4 justify-start ">
-          <img src={log} />
-          <p className="font-DungGeunMo text-heading-32 text-white">프로필 수정</p>
+      <div className="bg-gray-800 rounded-xl px-7 py-5 flex flex-col gap-4 justify-start items-start ">
+        <div className="flex justify-between w-full">
+          <div className="flex items-center gap-4">
+            <img src={log} />
+            <p className="font-DungGeunMo text-heading-32 text-white">프로필 수정</p>
+          </div>
+          <button className="border-gray-400 border-2 w-[20%] h-10 rounded-md text-gray-400 font-bold">수정하기</button>
         </div>
       </div>
     </div>
