0ef27b8ac317f0d9b20f29f325da964fc01fd97b
horang-e <rmsid1000@gmail.com>
2024-12-15 23:58:11 +0900

    Feat : 버튼 디자인 시스템 적용

--- src/spartaDesignSystem/SpartaButton.tsx
+++ src/spartaDesignSystem/SpartaButton.tsx
@@ -1,34 +1,76 @@
 type Props = {
   content: string;
+
   disabled?: boolean;
+
   type?: "standard" | "filled";
+
+  size?: "small" | "medium" | "large";
+
+  colorType?: "main" | "alert" | "error" | "grey";
+
   onClick: () => void;
 };
 
-const SpartaButton = ({ disabled = false, content, type = "standard", onClick }: Props) => {
+const SpartaButton = ({
+  disabled = false,
+
+  content,
+
+  type = "standard",
+
+  size = "large",
+
+  colorType = "main",
+
+  onClick,
+}: Props) => {
   // 동적 스타일을 적용하기 위한 함수
+
   const getButtonStyles = (): string => {
     // 기본 스타일
-    const baseStyles = "w-full h-14 text-title-18 rounded-lg transition-colors duration-200";
 
-    // disabled 상태일 때의 스타일
-    const disableStyles = {
-      standard: "bg-white border-2 border-gray-400 text-gray-400 ",
-      filled: "bg-gray-400 text-gray-900",
+    const baseStyles = "w-full rounded-lg transition-colors duration-200";
+
+    // size에 따른 스타일
+
+    const sizeStyles = {
+      small: "py-2 px-4 text-title-16",
+
+      medium: "py-3 px-5 text-title-18",
+
+      large: "py-4 px-6 text-title-22",
     };
 
-    // disabled 상태일 때
-    if (disabled) {
-      return `${baseStyles} ${disableStyles[type]} cursor-not-allowed`;
-    }
+    const colorDict = {
+      main: {
+        filled: "bg-primary-500 hover:bg-primary-300",
 
-    // type에 따른 스타일
-    const typeStyles = {
-      standard: "bg-white border-2 border-primary-500 text-primary-500 hover:border-primary-400 hover:text-primary-400",
-      filled: "bg-primary-500 text-primary-950 hover:bg-primary-400",
+        standard: "border-2 border-primary-500 text-primary-500 hover:text-primary-300 hover:border-primary-300",
+      },
+
+      alert: {
+        filled: "bg-alert-default hover:bg-alert-hover",
+
+        standard: "border-2 border-alert-default text-alert-default hover:text-alert-hover hover:border-alert-hover",
+      },
+
+      error: {
+        filled: "bg-error-default hover:bg-error-hover",
+
+        standard: "border-2 border-error-default text-error-default hover:text-error-hover hover:border-error-hover",
+      },
+
+      grey: {
+        filled: "bg-gray-500 hover:bg-gray-300",
+
+        standard: "border-2 border-gray-500 text-gray-500 hover:text-gray-300 hover:border-gray-300",
+      },
     };
 
-    return `${baseStyles} ${typeStyles[type]}`;
+    return `${baseStyles} ${colorDict[colorType][type]} ${sizeStyles[size]} ${
+      disabled && "cursor-not-allowed"
+    } hover:shadow-md`;
   };
 
   return (
