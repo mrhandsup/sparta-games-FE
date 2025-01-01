9d453b8f370f884ce1b0be7a57cd51810ca8bfc5
horang-e <rmsid1000@gmail.com>
2025-01-01 22:12:55 +0900

    Feat : 로딩 화면 추가..

--- src/page/Redirect.tsx
+++ src/page/Redirect.tsx
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { useEffect } from "react";
 import { useNavigate, useParams, useSearchParams } from "react-router-dom";
 import { discordLogin, googleLogin, kakaoLogin, naverLogin } from "../api/login";
+import loading from "../assets/common/loading.gif";
 
 const Redirect = () => {
   const { service } = useParams();
@@ -52,7 +53,11 @@ const Redirect = () => {
     }
   }, [data, isError]);
 
-  return <div>Redirect</div>;
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <img src={loading} className="w-20 h-20" />
+    </div>
+  );
 };
 
 export default Redirect;
