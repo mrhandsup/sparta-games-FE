8c019e1c378ba27420d6039e69d5f735121f610d
horang-e <rmsid1000@gmail.com>
2025-01-11 00:40:33 +0900

    Feat : add category hero img

--- src/components/categoryComponents/Title.tsx
+++ src/components/categoryComponents/Title.tsx
@@ -16,8 +16,9 @@ const Title = ({ category }: { category: string }) => {
   return (
     <section
       className={`sticky top-20 z-10 flex w-full pb-5 ${
-        isScrollActive ? "justify-start items-center mb-[325px] h-[150px]" : "justify-center items-end  h-[475px]"
-      } ease-in-out duration-200 [background:linear-gradient(180deg,rgba(23,23,23,0)_0%,rgb(23,23,23)_100%)] bg-cover bg-[50%_50%]`}
+        isScrollActive ? "justify-start items-center mb-[325px] h-[150px]" : "justify-center items-end h-[475px]"
+      } ease-in-out duration-200 bg-cover`}
+      style={{ backgroundImage: `url('/src/assets/category/${category}.png')` }}
     >
       <div className="font-DungGeunMo text-[64px] text-white">[{category}]</div>
     </section>
