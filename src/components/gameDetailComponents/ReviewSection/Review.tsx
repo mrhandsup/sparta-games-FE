d10972caf337240ae252f50975d8f65e9db08dfb
handsup <dhfma3394@naver.com>
2025-01-16 22:14:27 +0900

    Feat: 공용 페이지네이션 컴포넌트 구현

--- src/components/gameDetailComponents/ReviewSection/Review.tsx
+++ src/components/gameDetailComponents/ReviewSection/Review.tsx
@@ -1,31 +1,10 @@
-import { useState } from "react";
 import ReviewComents from "./ReviewComents";
-import { Pagination } from "@mui/material";
-import PaginationItem from "@mui/material/PaginationItem";
-
-import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 
 const Review = ({ gamePk }: { gamePk: number }) => {
   return (
     <section className="relative my-12">
       <ReviewComents gamePk={gamePk} />
       <div className="flex gap-8 my-5 mx-auto w-fit text-title-18 text-white"></div>
-      <div className="flex justify-center">
-        <Pagination
-          count={10}
-          shape="rounded"
-          renderItem={(item) => (
-            <PaginationItem
-              slots={{
-                previous: ArrowBackIcon,
-                next: ArrowForwardIcon,
-              }}
-              {...item}
-            />
-          )}
-        />
-      </div>
     </section>
   );
 };
--- src/components/gameDetailComponents/ReviewSection/ReviewComents.tsx
+++ src/components/gameDetailComponents/ReviewSection/ReviewComents.tsx
@@ -6,24 +6,26 @@ import { useQuery } from "@tanstack/react-query";
 import { TReviewResponse } from "../../../types";
 import useGameDetail from "../../../hook/gameDetailHook/useGameDetail";
 import { getGameMyReview, getGameReviews } from "../../../api/review";
+import SpartaPagination from "../../../spartaDesignSystem/SpartaPagination";
+import { useState } from "react";
 
 const ReviewComents = ({ gamePk }: { gamePk: number }) => {
+  const COUNT_PER_PAGE = 6;
+
+  const [currentPage, setCurrentPage] = useState(1);
+
   const { userData } = userStore();
 
   const { more, onClickMoreToggleHandler } = useGameDetail();
 
-  const onClickModalOpen = () => {
-    onClickMoreToggleHandler();
-  };
-
   const { data: reviewData } = useQuery<TReviewResponse>({
-    queryKey: ["reviews"],
-    queryFn: () => getGameReviews(gamePk),
+    queryKey: ["reviews", currentPage],
+    queryFn: () => getGameReviews(gamePk, currentPage, COUNT_PER_PAGE),
   });
 
   const { data: myReviewData } = useQuery<TReviewResponse>({
     queryKey: ["reviews", "my_review", gamePk],
-    queryFn: () => getGameMyReview(gamePk),
+    queryFn: () => getGameMyReview(gamePk, currentPage, COUNT_PER_PAGE),
     enabled: !!userData,
   });
 
@@ -31,6 +33,16 @@ const ReviewComents = ({ gamePk }: { gamePk: number }) => {
   const myReview = myReviewData?.results.my_review;
 
   const reviewsWithoutMyReview = allReviewData?.filter((review) => review.id !== myReview?.id);
+
+  const onClickModalOpen = () => {
+    onClickMoreToggleHandler();
+  };
+
+  const onChangePage = (event: React.ChangeEvent<unknown>, page: number) => {
+    setCurrentPage(page);
+  };
+
+  const isFirstPageVisible = currentPage !== 1 ? "hidden" : "";
 
   return (
     <>
@@ -46,24 +58,26 @@ const ReviewComents = ({ gamePk }: { gamePk: number }) => {
         <div className="grid grid-cols-3 gap-5">
           {userData ? (
             !myReview ? (
-              <>
-                <div
-                  onClick={onClickModalOpen}
-                  className="flex items-center justify-center gap-6 h-[189px] border border-solid border-primary-500 bg-gray-800 rounded-xl cursor-pointer"
-                >
-                  <img src={reviewRegister} />
-                  <p className="text-white font-DungGeunMo text-2xl">내 리뷰 등록하기</p>
-                </div>
-              </>
+              <div
+                onClick={onClickModalOpen}
+                className={`${isFirstPageVisible} flex items-center justify-center gap-6 h-[189px] border border-solid border-primary-500 bg-gray-800 rounded-xl cursor-pointer`}
+              >
+                <img src={reviewRegister} />
+                <p className="text-white font-DungGeunMo text-2xl">내 리뷰 등록하기</p>
+              </div>
             ) : (
-              <ReviewCard
-                onClickMoreToggleHandler={onClickMoreToggleHandler}
-                review={myReview}
-                isMyReview={!!myReview}
-              />
+              <div className={`${isFirstPageVisible}`}>
+                <ReviewCard
+                  onClickMoreToggleHandler={onClickMoreToggleHandler}
+                  review={myReview}
+                  isMyReview={!!myReview}
+                />
+              </div>
             )
           ) : (
-            <div className="flex items-center justify-center gap-6 px-11 h-[189px] border border-solid border-alert-default hover:border-alert-hover bg-gray-800 rounded-xl">
+            <div
+              className={`${isFirstPageVisible} flex items-center justify-center gap-6 px-11 h-[189px] border border-solid border-alert-default hover:border-alert-hover bg-gray-800 rounded-xl`}
+            >
               <img src={reviewRegister} />
               <p className="text-white font-DungGeunMo text-2xl text-center leading-none">
                 비회원은 리뷰등록이 불가능합니다.
@@ -75,7 +89,13 @@ const ReviewComents = ({ gamePk }: { gamePk: number }) => {
             <ReviewCard review={review} onClickMoreToggleHandler={onClickMoreToggleHandler} />
           ))}
         </div>
+        <SpartaPagination
+          dataTotalCount={userData ? myReviewData?.count : reviewData?.count}
+          countPerPage={COUNT_PER_PAGE}
+          onChangePage={onChangePage}
+        />
       </section>
+
       <ReviewRegisterModal
         gamePk={gamePk}
         more={more}
