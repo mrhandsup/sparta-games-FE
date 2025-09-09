6cab3802ac750bf972465b10b98f10c828861adf
handsup <dhfma3394@naver.com>
2025-09-09 22:59:35 +0900

    Fix: 읽은 알람 제목 그레이 처리, 알람 창 레이아웃 조정, 마이 페이지 팀빌딩 프로필 방어 코드 작성

--- src/components/headerComponents/AlarmModal.tsx
+++ src/components/headerComponents/AlarmModal.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 
 import { getAlarm } from "../../api/alarm";
@@ -45,15 +45,28 @@ const AlarmModal = ({ modalClose, handleAlarmClick }: props) => {
     }
   };
 
+  const lockScroll = useCallback(() => {
+    document.body.style.overflow = "hidden";
+  }, []);
+
+  const unlockScroll = useCallback(() => {
+    document.body.style.overflow = "";
+  }, []);
+
+  useEffect(() => {
+    lockScroll();
+    return () => unlockScroll();
+  }, [lockScroll, unlockScroll]);
+
   return (
     <div
       onClick={(e) => {
         e.stopPropagation();
       }}
-      className="absolute top-10 right-0 flex gap-5 py-5 px-2 w-[360px] h-[740px] overflow-y-auto bg-gray-800 border border-solid border-primary-500 shadow-primary rounded-[20px]"
+      className="absolute top-10 right-0 flex gap-5 py-5 px-2 w-[360px] max-h-[630px] overflow-y-auto bg-gray-800 border border-solid border-primary-500 shadow-primary rounded-[20px]"
     >
-      <div className="w-full py-2 px-6">
-        <h2 className="mb-6 text-xl text-white">
+      <div className="w-full px-6 overflow-y-auto">
+        <h2 className="mb-4 text-xl text-white">
           알림 <span className="text-[19px]">{alarmCount}</span>
         </h2>
 
@@ -67,12 +80,20 @@ const AlarmModal = ({ modalClose, handleAlarmClick }: props) => {
                 modalClose();
                 navigate(`/game-detail?id=${alarm.content_id}`);
               }}
-              className={`pb-4 text-sm border-solid border-gray-700 cursor-pointer ${
+              className={`pb-3 w-full text-sm border-solid border-gray-700 cursor-pointer ${
                 index !== alarms.length - 1 ? "border-b" : ""
               } ${alarm.is_read && "text-gray-400"} `}
             >
               <div className="flex justify-between items-center mb-1">
-                <p className={alarm.message.includes("반려") ? "text-error-default" : "text-primary-500"}>
+                <p
+                  className={
+                    alarm.is_read
+                      ? "text-gray-400"
+                      : alarm.message.includes("반려")
+                      ? "text-error-default"
+                      : "text-primary-500"
+                  }
+                >
                   {alarm.noti_type}
                 </p>
                 <span>{getTimeAgoInHours(alarm.create_dt)}</span>
@@ -80,19 +101,18 @@ const AlarmModal = ({ modalClose, handleAlarmClick }: props) => {
               <p className="font-Pretendard">{alarm.message}</p>
             </div>
           ))}
-
-          {nextUrl && (
-            <button
-              onClick={fetchNextPage}
-              disabled={isFetching}
-              className="flex items-center justify-center gap-2 w-full px-4 py-2 mb-8 border border-gray-100 rounded-lg
-              text-sm text-gray-200 hover:bg-gray-800 transition"
-            >
-              <span className="font-Pretendard">알림 더보기</span>
-              <img src={moreAlarm} alt="알림 더보기" />
-            </button>
-          )}
         </div>
+        {nextUrl && (
+          <button
+            onClick={fetchNextPage}
+            disabled={isFetching}
+            className="flex items-center justify-center gap-2 w-full px-4 py-2 border border-gray-100 rounded-lg
+              text-sm text-gray-200 hover:bg-gray-800 transition"
+          >
+            <span className="font-Pretendard">알림 더보기</span>
+            <img src={moreAlarm} alt="알림 더보기" />
+          </button>
+        )}
       </div>
     </div>
   );
